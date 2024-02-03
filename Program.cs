ee8a04362c130bd5a7fdb6dd74439ba4086f8c14
singhet <singhet@SCSWL4072>
2024-02-03 22:03:16 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        // tablica jednowymiarowa z cyframi od 1 do 100
+        //sumuje parzyste i nieparzyste- pętla foreach
+        int[] table = new int[100];
+        int sumEven = 0;
+        int sumOdd = 0;
+        for (int i = 0; i < table.Length; i++)
+        {
+            table[i] = i + 1;// wyświetlanie tablicy 1-100
+            //Console.WriteLine(table[i]);
+        }
+        foreach (int number in table)
+        {
+            if (number % 2 == 0)
+            {
+                sumEven += number;
+
+            }
+            else
+            {
+                sumOdd += number;
+            }
+        }
+        Console.WriteLine($"Sum of even numbers is: {sumEven}, sum od odd numbers is: {sumOdd}");
+        Console.ReadLine();
+    }
+}
